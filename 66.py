144242f77a066a09b69a38a7f7c79df9986d3bdd
Anees Vaid <aneesvaid@gmail.com>
2026-09-16 11:57:05 +0530

    Add files via upload

---
+++ 66.py
@@ -0,0 +1,10 @@
+# write a python program to print a hallow square using stars.
+n = int(input("Enter number of rows: "))
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
